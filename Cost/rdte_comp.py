0ddb528050c4a782080ac057ab47c023157ed2cd
JPeng45 <65366594+JPeng45@users.noreply.github.com>
2020-05-29 12:38:20 -0700

    Update rdte_comp.py

--- Cost/rdte_comp.py
+++ Cost/rdte_comp.py
@@ -18,11 +18,12 @@ class RDTEComp(ExplicitComponent):
         self.declare_partials('RDTE', 'V')
 
     def compute(self, inputs, outputs)
-
+        Q = self.options['Q']
+        FTA = self.options['FTA']
         We = inputs['We']
         V = inputs['V']
 
-        outputs['RDTE'] = 
+        outputs['RDTE'] = 2498*We**0.325*V**0.282*FTA**1.21 + 91.3*We**0.63*V**1.3 + 4.86*We**0.777*V**0.894*Q**0.163*115 + 5.99*We**0.777*V**0.696*Q**0.263*118.
 
     def compute_partials(self, inputs, partials):
         Q = self.options['Q']
@@ -31,5 +32,5 @@ class RDTEComp(ExplicitComponent):
         We = inputs['We']
         V = inputs['V']
 
-        partials['RDTE', 'We'] = 
-        partials['RDTE', 'V'] =  
+        partials['RDTE', 'We'] = 811.85*FTA**1.21*V**0.282/We**0.675+549.199*Q**0.263*V**0.696/We**0.223+434.265*Q**0.163*V**0.894/W**0.223+57.519*V**1.3/W**0.37.
+        partials['RDTE', 'V'] =  704.436*FTA**1.21*W**0.325/V**0.718+491.947*Q**0.263*W**0.777/V**0.304+499.657*Q**0.163*W**0.777/V**0.106+118.69*V**0.3*W**0.63.
