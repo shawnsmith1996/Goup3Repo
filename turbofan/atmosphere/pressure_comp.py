2fc1affec7797b407626e4a349e1134d37b94c75
magecliff96 <magecliff96@users.noreply.github.com>
2020-06-08 18:23:04 -0700

    debug

--- turbofan/atmosphere/pressure_comp.py
+++ turbofan/atmosphere/pressure_comp.py
@@ -11,12 +11,12 @@ class PressureComp(ArrayExplicitComponent):
     def setup(self):
         self.add_input('altitude')
         self.add_output('pressure_MPa')
-        self.declare_partials('pressure_MPa', 'altitude_km')
+        self.declare_partials('pressure_MPa', 'altitude')
         
 
 
     def compute(self, inputs, outputs):
-        h_m = inputs['altitude_km'] * 1e3
+        h_m = inputs['altitude'] * 1e3
 
         self.mask_arrays = get_mask_arrays(h_m)
 
@@ -25,7 +25,7 @@ class PressureComp(ArrayExplicitComponent):
         outputs['pressure_MPa'] = p_Pa / 1e6
 
     def compute_partials(self, inputs, partials):
-        h_m = inputs['altitude_km'] * 1e3
+        h_m = inputs['altitude'] * 1e3
         derivs = compute_pressure_derivs(h_m, *self.mask_arrays).flatten()
 
-        partials['pressure_MPa', 'altitude_km'] = derivs * 1e3 / 1e6
+        partials['pressure_MPa', 'altitude'] = derivs * 1e3 / 1e6
